5e65d26439fd468f8d94b4ddbfb3abc67041cf83
luka <lukakazuki1101@gmail.com>
2024-01-11 10:53:13 -0500

    it worketh lets gooo thanks graham

---
+++ src/Dogepro.java
@@ -0,0 +1,18 @@
+public class Dogepro extends GameObject{
+
+    public Dogepro(String[] data){
+        super(data);
+    }
+
+
+
+    @Override
+    public void showAbilityAnimation() {
+
+    }
+
+    @Override
+    public void showAttackAnimation() {
+
+    }
+}
